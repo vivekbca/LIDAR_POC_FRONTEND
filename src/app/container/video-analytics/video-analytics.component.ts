47b24d597d0b4f06dc52316744551d62335c4658
ratri33 <ratri@ispeck.co>
2023-02-03 11:39:16 +0530

    added

--- src/app/container/video-analytics/video-analytics.component.ts
+++ src/app/container/video-analytics/video-analytics.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ApiService } from 'src/app/service/api.service';
+import { GlobalService } from 'src/app/service/global.service';
 
 declare const openNav:any;
 
@@ -9,10 +16,33 @@ declare const openNav:any;
 })
 export class VideoAnalyticsComponent implements OnInit {
 
-  constructor() { }
+  submitted: boolean = false;
+  LidarForm:FormGroup;
 
-  ngOnInit(): void {
+  constructor(private apiService: ApiService, private globalservice: GlobalService, private spinner: NgxSpinnerService, private formBuilder: FormBuilder, private router: Router, private http: HttpClient) { }
+
+  private buildForm() {
+    this.LidarForm = this.formBuilder.group({
+      modelid: ['', [Validators.required]],
+      camid:['', [Validators.required]],
+      userid: ['', [Validators.required]]
+    });
+  }
+
+  ngOnInit() {
+    this.buildForm();
     new openNav();
+  }
+  get f() { return this.LidarForm.controls; }
+  RunModel()
+  {
+    this.submitted = true;
+    this.LidarForm.markAllAsTouched();
+    if (this.LidarForm.invalid) {
+      return;
+    }
+    if (this.LidarForm.valid) {
+    }
   }
 
 }
